7c74efb8a58c8e2a5bf4f9025471ee64ab938ad7
armut <abdullah.akalin@gazi.edu.tr>
2016-07-18 12:08:39 +0300

    Solution 11-2 updated.

--- chapter-11/11-2.py
+++ chapter-11/11-2.py
@@ -10,3 +10,6 @@ alex.color("blue")
 alex.forward(100)
 tess.forward(-100)
 wn.mainloop()
+
+# Because alex and tess correspond to the same turtle,
+# there is only one turtle, and hence, one color.
